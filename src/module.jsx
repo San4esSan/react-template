9267dc9b7aae233a621bbb94a39c6bef1c1a731e
mister.San4es <mister.San4es@list.ru>
2022-07-13 17:57:52 +0400

    create react-template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import yan from './img/yan.jpg'
+
+const Module = () => (
+  <>
+    <h1>Hello, React</h1>
+    <img width={200} src={yan} />
+  </>
+);
+
+export default Module;
